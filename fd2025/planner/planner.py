d96bb978383b1a766601e9478f9360d523eb910e
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-03-28 11:30:40 +0900

    Infer feasbility after hypo-remove obstacles

--- fd2025/planner/planner.py
+++ fd2025/planner/planner.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
+from itertools import combinations
+from typing import List, Optional
 
 import numpy as np
+import torch
+import torch.nn as nn
+from hifuku.batch_network import BatchFCN
+from hifuku.core import SolutionLibrary
+from hifuku.domain import JSKFridge
+from hifuku.script_utils import load_library
+from plainmp.ompl_solver import OMPLSolver, OMPLSolverConfig
 from rpbench.articulated.pr2.jskfridge import JskFridgeVerticalReachingTask
+from rpbench.articulated.vision import create_heightmap_z_slice
 from rpbench.articulated.world.jskfridge import get_fridge_model
 from rpbench.articulated.world.utils import CylinderSkelton
 from skrobot.coordinates import Coordinates, rpy_angle
@@ -71,28 +81,171 @@ def create_task_param(detection: FridgeEnvDetection, target_coords: Coordinates)
     return task_param
 
 
+class ActionEdge:
+    ...
+
+
+class ReachAction(ActionEdge):
+    ...
+
+
+@dataclass
+class MoveCanAction(ActionEdge):
+    idx_can: int
+    # detail
+    start_pos: Optional[np.ndarray] = None
+    target_pos: Optional[np.ndarray] = None
+
+
+@dataclass
+class MoveBaseAction(ActionEdge):
+    # detail
+    start_pose: np.ndarray
+    target_pose: np.ndarray
+
+
+class Node:
+    state: np.ndarray
+    parent: Optional["Node"]
+    action: Optional[ActionEdge]
+
+
+class FeasibilityCheckerBatchImageJit:
+    ae_modeL_shared: torch.jit.ScriptModule
+
+    def __init__(self, n_batch: int):
+        lib: SolutionLibrary = load_library(JSKFridge, "cuda", postfix="0.2")
+        self.dummy_encoded = torch.zeros(n_batch, 200).float().cuda()
+        self.biases = torch.tensor(lib.biases).float().cuda()
+        self.max_admissible_cost = lib.max_admissible_cost
+
+        # ae part
+        dummy_input = torch.zeros(n_batch, 1, 112, 112).float().cuda()
+        traced = torch.jit.trace(lib.ae_model_shared.eval(), (dummy_input,))
+        self.ae_model_shared = torch.jit.optimize_for_inference(traced)
+
+        # vector part
+        linear_list = []
+        expander_list = []
+        for pred in lib.predictors:
+            linear_list.append(pred.linears)
+            expander_list.append(pred.description_expand_linears)
+        fcn_linears_batch = BatchFCN(linear_list).cuda()
+        fcn_expanders_batch = BatchFCN(expander_list).cuda()
+
+        class Tmp(nn.Module):
+            def __init__(self, fcn_linears, fcn_expanders, n):
+                super().__init__()
+                self.fcn_linears = fcn_linears
+                self.fcn_expanders = fcn_expanders
+                self.n = n
+
+            def forward(self, bottlenecks, descriptor):
+                # bottlenecks: (n_batch, n_latent)
+                n_batch = bottlenecks.shape[0]
+                expanded = self.fcn_expanders(descriptor.unsqueeze(0))
+                expanded_repeat = expanded.repeat(n_batch, 1, 1)
+                bottlenecks_repeat = bottlenecks.unsqueeze(1).repeat(1, self.n, 1)
+                concat = torch.cat((bottlenecks_repeat, expanded_repeat), dim=2)
+                tmp = self.fcn_linears(concat)
+                return tmp.squeeze(2)
+
+            def cuda(self):
+                self.fcn_linears.cuda()
+                self.fcn_expanders.cuda()
+                return super().cuda()
+
+        tmp = Tmp(fcn_linears_batch, fcn_expanders_batch, len(lib.predictors)).cuda()
+        dummy_input = (torch.zeros(n_batch, 200).float().cuda(), torch.zeros(7).float().cuda())
+        traced = torch.jit.trace(tmp, dummy_input)
+        self.batch_predictor = torch.jit.optimize_for_inference(traced)
+
+        # warm up
+        vector = np.random.randn(7).astype(np.float32)
+        hmaps = [np.random.randn(112, 112).astype(np.float32) for _ in range(10)]
+        for _ in range(10):
+            self.infer(vector, hmaps)
+
+    def infer(self, vector: np.ndarray, mat_lits: List[np.ndarray]):
+        n_batch_actual = len(mat_lits)
+        vector = torch.from_numpy(vector).float().cuda()
+
+        mats = torch.stack([torch.from_numpy(mat).float() for mat in mat_lits]).unsqueeze(1).cuda()
+        encoded = self.ae_model_shared.forward(mats)
+        self.dummy_encoded[:n_batch_actual] = encoded
+
+        costs = self.batch_predictor(self.dummy_encoded, vector)
+        cost_calibrated = costs[:n_batch_actual] + self.biases
+        min_costs, min_indices = torch.min(cost_calibrated, dim=1)
+        return (
+            min_costs.cpu().detach().numpy() < self.max_admissible_cost,
+            min_indices.cpu().detach().numpy(),
+        )
+
+
+class TampSolver:
+    def __init__(self):
+        self._checker = FeasibilityCheckerBatchImageJit(20)
+
+    def solve(self, task_param: np.ndarray):
+        task_init = JskFridgeVerticalReachingTask.from_task_param(task_param)
+        self._hypothetical_check(task_init)
+
+    def _hypothetical_check(self, task: JskFridgeVerticalReachingTask) -> bool:
+        region = get_fridge_model().regions[task.world.attention_region_index]
+        obstacles = task.world.get_obstacle_list()
+        n_obs = len(obstacles)
+        hmap_list = []
+        for n in range(n_obs, 0, -1):
+            for comb in combinations(obstacles, n):
+                lst = list(comb)
+                hmap = create_heightmap_z_slice(region.box, lst, 112)
+                hmap_list.append(hmap)
+        feasibilities, _ = self._checker.infer(task.description, hmap_list)
+
+    def is_feasible(self, task: JskFridgeVerticalReachingTask) -> bool:
+        problem = task.export_problem()
+        conf = OMPLSolverConfig(n_max_call=1000000, timeout=0.1, n_max_ik_trial=1000)
+        solver = OMPLSolver(conf)
+        ret = solver.solve(problem)
+        return ret.traj is None
+
+
 if __name__ == "__main__":
     node = PerceptionDebugNode("20250326_082328")
     detection = node.percept()
-    v = detection.visualize()
     ax = Axis()
-    ax.translate([0.75, +0.4, 1.1])  # easy
+    ax.translate([0.3, -0.1, 1.05])  # easy
     task_param = create_task_param(detection, ax.copy_worldcoords())
-    task = JskFridgeVerticalReachingTask.from_task_param(task_param)
+    solver = TampSolver()
+    from pyinstrument import Profiler
 
-    from rpbench.articulated.pr2.jskfridge import AV_INIT
-    from skrobot.models import PR2
-    from skrobot.viewers import PyrenderViewer
+    profiler = Profiler()
+    profiler.start()
+    solver.solve(task_param)
+    profiler.stop()
+    print(profiler.output_text(unicode=True, color=True, show_all=False))
 
-    v = PyrenderViewer()
-    task.world.visualize(v)
-    pr2 = PR2(use_tight_joint_limit=False)
-    pr2.angle_vector(AV_INIT)
-    base_pose = task.description[-3:]
-    pr2.translate(np.hstack([base_pose[:2], 0.0]))
-    pr2.rotate(base_pose[2], "z")
-    v.add(pr2)
-    v.show()
-    import time
+    debug = False
+    if debug:
+        v = detection.visualize()
+        task = JskFridgeVerticalReachingTask.from_task_param(task_param)
+        # ret = task.solve_default()
 
-    time.sleep(1000)
+        from rpbench.articulated.pr2.jskfridge import AV_INIT
+        from skrobot.models import PR2
+        from skrobot.viewers import PyrenderViewer
+
+        v = PyrenderViewer()
+        task.world.visualize(v)
+        pr2 = PR2(use_tight_joint_limit=False)
+        pr2.angle_vector(AV_INIT)
+        base_pose = task.description[-3:]
+        pr2.translate(np.hstack([base_pose[:2], 0.0]))
+        pr2.rotate(base_pose[2], "z")
+        v.add(pr2)
+        v.add(ax)
+        v.show()
+        import time
+
+        time.sleep(1000)
